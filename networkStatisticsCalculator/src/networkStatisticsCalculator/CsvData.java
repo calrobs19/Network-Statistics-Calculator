479bd18d7ccd80ec3edaeddda064b08495abfde0
calrobs19 <callumliam@outlook.com>
2024-01-08 19:57:40 +0000

    moved data folder inside project directory, we created a CSV data class and create CSV object and print contents

---
+++ networkStatisticsCalculator/src/networkStatisticsCalculator/CsvData.java
@@ -0,0 +1,30 @@
+package networkStatisticsCalculator;
+
+public class CsvData {
+	
+	private String date;
+	private int localIP;
+	private int remoteASN;
+	private int flows;
+	
+	public CsvData(String date, int localIP, int remoteASN, int flows) {
+		
+	this.date = date;
+	this.localIP = localIP;
+	this.remoteASN = remoteASN;
+	this.flows = flows;
+	
+	}
+	
+	@Override    
+	public String toString() {
+		return "CsvData{" +
+			"date='" + date + '\'' +
+			", localIP=" + localIP +
+			", remoteASN=" + remoteASN +
+			", flows=" + flows +'}';    
+		
+	
+
+}
+}
---
+++ networkStatisticsCalculator/src/networkStatisticsCalculator/Main.java
@@ -0,0 +1,11 @@
+package networkStatisticsCalculator;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+		CsvData csvdata = new CsvData("8/1/2024",20,100,8);
+        System.out.println(csvdata.toString());
+	}
+
+}
