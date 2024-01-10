7fa9c1ed4db05acddf982f6fbbef167f276d6d20
calrobs19 <callumliam@outlook.com>
2024-01-10 20:55:38 +0000

    instead of printing all the data to the console window we have only printed the most extreme values

--- networkStatisticsCalculator/src/networkStatisticsCalculator/Main.java
+++ networkStatisticsCalculator/src/networkStatisticsCalculator/Main.java
@@ -1,36 +1,19 @@
 package networkStatisticsCalculator;
 
-import java.io.*;
-import java.util.HashMap;
-import java.io.BufferedReader;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 	public static void main(String[] args) {
+		// reading CSV file and storing as a list of CSV data objects
 		String file = "data/PCAP Data.csv";
 		FileUtilities fileUtilities = new FileUtilities();
 		List<CsvData> dataList = fileUtilities.readCsvFile(file);
 
+		// initialise network stats object create file and write the network stats to
+		// file with datalist
 		NetworkStatistics networkStats = new NetworkStatistics();
-		HashMap<Integer, Integer> connectionsPerUser = networkStats.connectionsPerUser(dataList);
-
-		for (HashMap.Entry<Integer, Integer> entry : connectionsPerUser.entrySet()) {
-			int localIP = entry.getKey();
-			int totalFlows = entry.getValue();
-			System.out.println("LocalIP: " + localIP + ", Total Flows " + totalFlows);
-		}
-		HashMap<Integer, Integer> totalNumberOfconnectionsPerASN = networkStats
-				.totalNumberOfconnectionsPerASN(dataList);
-
-		for (HashMap.Entry<Integer, Integer> entry : totalNumberOfconnectionsPerASN.entrySet()) {
-			int remoteASN = entry.getKey();
-			int totalFlows = entry.getValue();
-			System.out.println("remote ASN: " + remoteASN + ", Total Flows " + totalFlows);
-		}
-		networkStats.totalNumberOfConnectionsPerWeek(dataList);
-
+		networkStats.connectionsPerUser(dataList);
+		//fileUtilities.createFile();
+		//fileUtilities.writeToFile(networkStats, dataList);
 	}
-
-	
 }
--- networkStatisticsCalculator/src/networkStatisticsCalculator/NetworkStatistics.java
+++ networkStatisticsCalculator/src/networkStatisticsCalculator/NetworkStatistics.java
@@ -3,6 +3,8 @@ package networkStatisticsCalculator;
 import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 import java.time.LocalDate;
 
 /**
@@ -16,7 +18,7 @@ public class NetworkStatistics {
 	 * @param List of CsvData
 	 * @return HashMap of Integer, Integer
 	 */
-	public HashMap<Integer, Integer> connectionsPerUser(List<CsvData> dataList) {
+	public List<Map.Entry<Integer, Integer>> connectionsPerUser(List<CsvData> dataList) {
 		HashMap<Integer, Integer> userConnections = new HashMap<Integer, Integer>();
 
 		for (CsvData data : dataList) {
@@ -25,24 +27,28 @@ public class NetworkStatistics {
 
 			userConnections.merge(localIP, flows, Integer::sum);
 		}
-		return userConnections;
+		// sorted all flows in descending order and selecting the first 3 values 
+		List<Map.Entry<Integer, Integer>> topFlowsPerIP = userConnections.entrySet().stream()
+				.sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed()).limit(3)
+				.collect(Collectors.toList());
+		return topFlowsPerIP;
 	}
-
-	public void totalNumberOfConnectionsPerWeek(List<CsvData> dataList) {
+   
+	public List<Map.Entry<String, Integer>> totalNumberOfConnectionsPerWeek(List<CsvData> dataList) {
 		HashMap<String, Integer> connectionsPerWeek = new HashMap<>();
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
 
 		for (CsvData data : dataList) {
 			String weekStartDate = getWeekStartDate(data.getDate(), formatter);
 			int flows = data.getFlows();
-
-			connectionsPerWeek.merge(weekStartDate, flows, Integer::sum);
+			connectionsPerWeek.merge(weekStartDate, flows, Integer::sum);			
 		}
-		for (HashMap.Entry<String, Integer> entry : connectionsPerWeek.entrySet()) {
-			String weekStartDate = entry.getKey();
-			int totalFlows = entry.getValue();
-			System.out.println("Week starting on " + weekStartDate + ": Total Flows" + totalFlows);
-		}
+		// sorted all flows in descending order alongside the week and selecting the 3 greatest values 
+		List<Map.Entry<String, Integer>> topFlowsPerWeek = connectionsPerWeek.entrySet().stream()
+				.sorted(Map.Entry.<String, Integer>comparingByValue().reversed()).limit(3)
+				.collect(Collectors.toList());
+		return topFlowsPerWeek;
+		
 	}
 
 	private String getWeekStartDate(String date, DateTimeFormatter formatter) {
@@ -51,7 +57,7 @@ public class NetworkStatistics {
 		return weekStartDate.format(formatter);
 	}
 
-	public HashMap<Integer, Integer> totalNumberOfconnectionsPerASN(List<CsvData> dataList) {
+	public List<Map.Entry<Integer, Integer>> totalNumberOfconnectionsPerASN(List<CsvData> dataList) {
 		HashMap<Integer, Integer> connectionsPerASN = new HashMap<Integer, Integer>();
 
 		for (CsvData data : dataList) {
@@ -60,7 +66,10 @@ public class NetworkStatistics {
 
 			connectionsPerASN.merge(remoteASN, flows, Integer::sum);
 		}
-		return connectionsPerASN;
+		// sorted all flows in descending order and selecting the first 3 values 
+		List<Map.Entry<Integer, Integer>> topFlowsPerASN = connectionsPerASN.entrySet().stream()
+				.sorted(Map.Entry.<Integer, Integer>comparingByValue().reversed()).limit(3)
+				.collect(Collectors.toList());
+		return topFlowsPerASN;
 	}
-
 }
